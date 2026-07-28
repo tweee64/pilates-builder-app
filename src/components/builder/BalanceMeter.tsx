a61c873655c4b030e40487903d1ef5d1dd30053c
Thu Thuy Pham <phamthuthuy498@gmail.com>
2026-07-28 17:21:40 +0800

    :hammer: fix the balance meter

--- src/components/builder/BalanceMeter.tsx
+++ src/components/builder/BalanceMeter.tsx
@@ -47,7 +47,7 @@ function MatBalanceMeter({ items }: { items: ClassItem[] }) {
         <div className="flex" style={{ width: `${flexPct}%` }} />
         <div className="ext" style={{ width: `${100 - flexPct}%` }} />
       </div>
-      <div className={`tip${advisory ? "show" : ""}`}>
+      <div className={`tip${advisory ? " show" : ""}`}>
         <span className="ic">◆</span>
         <span>{advisory}</span>
       </div>
@@ -72,7 +72,7 @@ function ReformerAdvisories({ items }: { items: ClassItem[] }) {
 
   return (
     <div className="balance">
-      <div className={`tip${advisory ? "show" : ""}`}>
+      <div className={`tip${advisory ? " show" : ""}`}>
         <span className="ic">◆</span>
         <span>{advisory}</span>
       </div>
